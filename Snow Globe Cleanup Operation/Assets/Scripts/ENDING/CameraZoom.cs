68ce89e28cbfcc51e9f1a027021ccff12fba137f
H8bubble <138587585+H8bubble@users.noreply.github.com>
2025-02-27 09:55:32 +0900

    [ENDING]feature: sad ending cut scene

--- Snow Globe Cleanup Operation/Assets/Scripts/ENDING/CameraZoom.cs
+++ Snow Globe Cleanup Operation/Assets/Scripts/ENDING/CameraZoom.cs
@@ -1,3 +1,4 @@
+using TMPro; // TextMeshPro 네임스페이스 추가
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,27 +8,34 @@ public class CameraZoom : MonoBehaviour
     public GameObject SnowballImage;
     public GameObject buttonPanel;
     public Button SnowballButton;
+    public TMP_Text text;
     public float zoomSize = 2f;
     public float zoomSpeed = 2f;
+    public float blinkSpeed = 1f;
 
     private Vector3 targetPosition;
     private float originalSize;
     private bool isZooming = false;
+    private bool isBlinking = false;
 
     void Start()
     {
         if (mainCamera == null)
             mainCamera = Camera.main;
         originalSize = mainCamera.orthographicSize;
-        SnowballImage.gameObject.SetActive(false);
+        SnowballImage.SetActive(false);
         buttonPanel.SetActive(false);
+        text.gameObject.SetActive(false); // 초기 비활성화
+        StartBlinking();
     }
 
     public void ZoomToTarget(Transform target)
     {
         Vector3 worldPosition = target.position;
-        targetPosition = new Vector3(target.position.x, target.position.y, mainCamera.transform.position.z);
+        targetPosition = new Vector3(worldPosition.x, worldPosition.y, mainCamera.transform.position.z);
         SnowballImage.SetActive(true);
+        isBlinking = false;
+        text.gameObject.SetActive(false);
         isZooming = true;
     }
 
@@ -36,7 +44,6 @@ public class CameraZoom : MonoBehaviour
         if (isZooming)
         {
             mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, targetPosition, Time.deltaTime * zoomSpeed);
-
             mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, zoomSize, Time.deltaTime * zoomSpeed);
 
             if (Vector3.Distance(mainCamera.transform.position, targetPosition) < 0.1f && Mathf.Abs(mainCamera.orthographicSize - zoomSize) < 0.1f)
@@ -44,6 +51,23 @@ public class CameraZoom : MonoBehaviour
                 isZooming = false;
                 buttonPanel.SetActive(true);
             }
+        }
+    }
+
+    private void StartBlinking()
+    {
+        isBlinking = true;
+        text.gameObject.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+        if (isBlinking)
+        {
+            float alpha = Mathf.PingPong(Time.time * blinkSpeed, 1.0f);
+            Color textColor = text.color;
+            textColor.a = alpha;
+            text.color = textColor;
         }
     }
 }
