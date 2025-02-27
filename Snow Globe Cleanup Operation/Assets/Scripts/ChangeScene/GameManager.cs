4140df640f216f95fd266076f14e3b2bbad657a6
박예원 <voo_247@naver.com>
2025-02-27 20:28:22 +0900

    Revert "[GAME/MAIN]refac: STAGE D 제외"
    This reverts commit cec306cb33a70b43d7f0b9dd4b15df6a9226778e, reversing
    changes made to 547a49f78dc923b2362a6160cd4f4312241eefba.

--- Snow Globe Cleanup Operation/Assets/Scripts/ChangeScene/GameManager.cs
+++ Snow Globe Cleanup Operation/Assets/Scripts/ChangeScene/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     
     void Start()
     {
-        if (PlayerPrefs.GetInt("STAGEA", 0) == 1 && PlayerPrefs.GetInt("STAGEB", 0) == 1 && PlayerPrefs.GetInt("STAGEC", 0) == 1 && PlayerPrefs.GetInt("STAGEE", 0) == 1 && gameTimer.now > 0)
+        if (PlayerPrefs.GetInt("STAGEA", 0) == 1 && PlayerPrefs.GetInt("STAGEB", 0) == 1 && PlayerPrefs.GetInt("STAGEC", 0) == 1 && PlayerPrefs.GetInt("STAGED", 0) == 1 && PlayerPrefs.GetInt("STAGEE", 0) == 1 && PlayerPrefs.GetInt("STAGEE", 0) == 1 && gameTimer.now > 0)
         {
             Timer.Instance?.ResetTimer();
             SceneManager.LoadScene("EndingStory_HAPPY");
@@ -29,10 +29,10 @@ public class GameManager : MonoBehaviour
         UnityEngine.SceneManagement.SceneManager.LoadScene("STAGE_C");
     }
 
-    //public void LoadMiniGameD()
-    //{
-    //    UnityEngine.SceneManagement.SceneManager.LoadScene("STAGE_D");
-    //}
+    public void LoadMiniGameD()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("STAGE_D");
+    }
 
     public void LoadMiniGameE()
     {
