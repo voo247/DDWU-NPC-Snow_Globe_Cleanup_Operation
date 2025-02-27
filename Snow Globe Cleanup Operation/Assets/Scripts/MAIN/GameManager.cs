1a5f4f1400f1fac6bcaed2e06f6b9ba96a0c8f98
H8bubble <138587585+H8bubble@users.noreply.github.com>
2025-02-27 14:16:56 +0900

    [GAME/MAIN]feature:main timer 구현

--- Snow Globe Cleanup Operation/Assets/Scripts/MAIN/GameManager.cs
+++ Snow Globe Cleanup Operation/Assets/Scripts/MAIN/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Security.Cryptography;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
--- Snow Globe Cleanup Operation/Assets/Scripts/System/Timer.cs
+++ Snow Globe Cleanup Operation/Assets/Scripts/System/Timer.cs
@@ -10,9 +10,21 @@ public class Timer : MonoBehaviour
     float now;                      //! 슬라이더에 표현될 현재 남은 시간
     public string endScene;         //! 게임 오버 씬
 
+    private void Awake()
+    {
+        // 타이머 객체가 씬 전환 시에도 파괴되지 않도록 설정
+        DontDestroyOnLoad(gameObject);
+    }
+
     private void Start()
     {
         now = endTime;
+
+        // 이미 저장된 타이머 값이 있으면 복원
+        if (PlayerPrefs.HasKey("TimerValue"))
+        {
+            now = PlayerPrefs.GetFloat("TimerValue");
+        }
     }
 
     void FixedUpdate()
@@ -23,10 +35,14 @@ public class Timer : MonoBehaviour
             {
                 now -= Time.deltaTime;
                 timeSlider.value = now / endTime;
+
+                // 타이머 값 저장
+                PlayerPrefs.SetFloat("TimerValue", now);
+                PlayerPrefs.Save();
             }
             else
             {
-                SceneManager.LoadScene(endScene);
+                SceneManager.LoadScene("BADENDING");
             }
         }
     }
